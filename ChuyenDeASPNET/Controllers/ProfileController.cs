f60de23e581813a8685e61e3d5f0e425c227357f
nguyenthanhcong220505 <168881783+nguyenthanhcong220505@users.noreply.github.com>
2024-12-04 19:49:04 +0700

    first commit

---
+++ ChuyenDeASPNET/Controllers/ProfileController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChuyenDeASPNET.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult Orders()
+        {
+            return View();
+        }
+        public ActionResult Address()
+        {
+            return View();
+        }
+        public ActionResult Main()
+        {
+            return View();
+        }
+        public ActionResult Seller()
+        {
+            return View();
+        }
+        public ActionResult Setting()
+        {
+            return View();
+        }
+        public ActionResult Wishlist()
+        {
+            return View();
+        }
+    }
+}
